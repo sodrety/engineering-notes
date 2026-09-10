da507e952a54c7eacb7190519423d6db6f64a563
sodrety <dederamdani52@gmail.com>
2026-09-10 19:17:26 +0800

    Configure blog for deram.my.id

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://blog.derammd.com',
+	site: 'https://deram.my.id',
 	integrations: [mdx(), sitemap()],
 });
